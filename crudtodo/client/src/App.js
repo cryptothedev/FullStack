1301ffd9467f97e4e05b23792b4764144eb10e78
cryptodev <crypto.the.dev@gmail.com>
2022-08-16 06:54:36 +0700

    chore: remove node_modules

--- crudtodo/client/src/App.js
+++ crudtodo/client/src/App.js
@@ -1,33 +1,36 @@
 //app.js
 import React from 'react'
-import './App.css';
+import './App.css'
 import Axios from 'axios'
 import { useState } from 'react'
 
 function App() {
-  
-  const [input,setInput] = useState("");
-  const [todos,setTodos] = useState([]);
-  const [editTodos,seteditTodos] = useState("");
+  const [input, setInput] = useState('')
+  const [todos, setTodos] = useState([])
+  const [editTodos, seteditTodos] = useState('')
 
   const getTodos = () => {
-    Axios.get(`https://todo-list-crud-api-jk.herokuapp.com/`).then((response)=> {
-      setTodos(response.data);
-    });
-  };
+    Axios.get(`https://todo-list-crud-api-jk.herokuapp.com/`).then(
+      (response) => {
+        setTodos(response.data)
+      }
+    )
+  }
 
   const getInput = () => {
-    Axios.post(`https://todo-list-crud-api-jk.herokuapp.com/addtodos`,{
-      input: input
+    Axios.post(`https://todo-list-crud-api-jk.herokuapp.com/addtodos`, {
+      input: input,
     })
-  };
-  
+  }
+
   const completetodos = (taskid) => {
     Axios.put(`https://todo-list-crud-api-jk.herokuapp.com/complete/${taskid}`)
   }
 
   const deletetodos = (taskid) => {
-    Axios.delete(`https://todo-list-crud-api-jk.herokuapp.com/deletetodos/${taskid}`)
+    Axios.delete(
+      `https://todo-list-crud-api-jk.herokuapp.com/deletetodos/${taskid}`
+    )
   }
 
   const editclick = (taskid) => {
@@ -35,85 +38,108 @@ function App() {
   }
 
   const editsumbit = (taskid) => {
-    Axios.put(`https://todo-list-crud-api-jk.herokuapp.com/editsumbit/${taskid}`,{
-      editTodos: editTodos
-    })
+    Axios.put(
+      `https://todo-list-crud-api-jk.herokuapp.com/editsumbit/${taskid}`,
+      {
+        editTodos: editTodos,
+      }
+    )
   }
 
-  return ( 
-    <div className='container'>
-      <div className='app-wrapper'>
-          <div className='header'><h2>Todolist</h2></div>
-          <div >
-              <form>
-              <input type="text" className='task-input'
-              placeholder = "List your todo here..."
-              onChange={(event)=>{
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div className="header">
+          <h2>Todolist</h2>
+        </div>
+        <div>
+          <form>
+            <input
+              type="text"
+              className="task-input"
+              placeholder="List your todo here..."
+              onChange={(event) => {
                 setInput(event.target.value)
               }}
-              />
-              <button className='button-add' type='submit'
-              onClick={getInput}>sumbit</button>
-              </form>
-          </div>
-          
+            />
+            <button className="button-add" type="submit" onClick={getInput}>
+              sumbit
+            </button>
+          </form>
+        </div>
 
-            <div>
-              {getTodos()}
-              {todos.map((val)=>{
-                return(
-                      <li className='list-item'>
-                        {val.editON % 2 === 0 
-                        ? 
-                        <div>
-                        <p className={val.complete % 2 === 0 ?'list':'complete list'}>
-                        {val.tasks}</p>  
-                        </div>
-                        :  
-                        <div>
-                        <input className='list' type="text" 
-                        placeholder = {val.tasks}
-                        onChange={(event)=>{
-                          seteditTodos(event.target.value)}
-                        }
-                        >
-                        </input>
-                        </div>                        
-                        }
-                        {val.editON % 2 === 0 ? 
-                      <div>
-                      <button className='button-complete'
-                       onClick={()=>{completetodos(val.taskid)}}>
-                       <i class="fa-regular fa-circle-check"></i>
-                       </button>
-                       <button className='button-edit'
-                       onClick={()=>{editclick(val.taskid)}}>
-                       <i class="fa-regular fa-pen-to-square"></i>
-                       </button>
-                       <button className='button-delete'
-                       onClick={()=>{deletetodos(val.taskid)}}>
-                       <i class="fa-regular fa-trash-can"></i>
-                       </button>
-                       </div>
-                       : 
-                       <button className='button-editsumbit'
-                       type='submit'
-                       onClick={()=>{editsumbit(val.taskid)}}>
-                       <i class="fa-solid fa-pen-to-square"></i>
-                       </button>
-                        }
-                       
-                      </li>
-                )
-              })}
-            </div>
-
-          
+        <div>
+          {getTodos()}
+          {todos.map((val) => {
+            return (
+              <li className="list-item">
+                {val.editON % 2 === 0 ? (
+                  <div>
+                    <p
+                      className={
+                        val.complete % 2 === 0 ? 'list' : 'complete list'
+                      }
+                    >
+                      {val.tasks}
+                    </p>
+                  </div>
+                ) : (
+                  <div>
+                    <input
+                      className="list"
+                      type="text"
+                      placeholder={val.tasks}
+                      onChange={(event) => {
+                        seteditTodos(event.target.value)
+                      }}
+                    ></input>
+                  </div>
+                )}
+                {val.editON % 2 === 0 ? (
+                  <div>
+                    <button
+                      className="button-complete"
+                      onClick={() => {
+                        completetodos(val.taskid)
+                      }}
+                    >
+                      <i class="fa-regular fa-circle-check"></i>
+                    </button>
+                    <button
+                      className="button-edit"
+                      onClick={() => {
+                        editclick(val.taskid)
+                      }}
+                    >
+                      <i class="fa-regular fa-pen-to-square"></i>
+                    </button>
+                    <button
+                      className="button-delete"
+                      onClick={() => {
+                        deletetodos(val.taskid)
+                      }}
+                    >
+                      <i class="fa-regular fa-trash-can"></i>
+                    </button>
+                  </div>
+                ) : (
+                  <button
+                    className="button-editsumbit"
+                    type="submit"
+                    onClick={() => {
+                      editsumbit(val.taskid)
+                    }}
+                  >
+                    <i class="fa-solid fa-pen-to-square"></i>
+                  </button>
+                )}
+              </li>
+            )
+          })}
+        </div>
       </div>
     </div>
-  );
-
-
+  )
 }
 
-export default App;
+export default App
--- crudtodo/server/server.js
+++ crudtodo/server/server.js
@@ -2,97 +2,107 @@
 var express = require('express')
 var cors = require('cors')
 const mysql = require('mysql')
-var port_number = process.env.PORT || 3000 ;
+var port_number = process.env.PORT || 3000
 const database = 'n078766yizzhxg00'
 var server = express()
 server.use(cors())
 server.use(express.json())
-server.listen(port_number);
+server.listen(port_number)
 
 const connection = mysql.createConnection({
-    host:'XXX',
-    port: 'XXX',
-    user:'XXX',
-    password:'XXX',
-    database:'XXX'
+  host: 'XXX',
+  port: 'XXX',
+  user: 'XXX',
+  password: 'XXX',
+  database: 'XXX',
 })
 
-server.post('/addtodos',(req,res)=>{
-    const input = req.body.input;
-    if (input != "") {
-    connection.query(`INSERT INTO ${database}.todotask (tasks) VALUES (?);`,[input],
-    (err,result)=>{
-        if(err) {
-            console.log(err)
-        } else {
-            res.send("complelete")
-        }
-    }
-    )    
-    } else return
-    
-})
-
-server.get('/',(req,res)=>{
-    connection.query(`SELECT * FROM ${database}.todotask;`,(err,result)=>{
+server.post('/addtodos', (req, res) => {
+  const input = req.body.input
+  if (input != '') {
+    connection.query(
+      `INSERT INTO ${database}.todotask (tasks) VALUES (?);`,
+      [input],
+      (err, result) => {
         if (err) {
-            console.log(err);
+          console.log(err)
         } else {
-            res.send(result);
+          res.send('complelete')
         }
-    });
-});
-
-server.put('/complete/:taskid',(req,res)=>{
-    const taskid = req.params.taskid;
-    connection.query(`UPDATE ${database}.todotask SET complete = complete +1 WHERE taskid = ?`,taskid,
-    (err,result)=>{
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    })
-})
-
-server.delete('/deletetodos/:taskid',(req,res)=>{
-    const taskid = req.params.taskid;
-    connection.query(`DELETE FROM ${database}.todotask WHERE (taskid = ?)`, taskid ,
-    (err,result)=>{
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    })
-})
-
-server.put('/editsumbit/:taskid',(req,res)=>{
-    const taskid = req.params.taskid;
-    const editTodos = req.body.editTodos;
-    if (editTodos !== "") {
-        connection.query(`UPDATE ${database}.todotask SET tasks = ?, editON = editON +1 WHERE taskid = ?;`,
-        [editTodos,taskid],
-        (err,result)=>{
-            if (err) {
-                console.log(err);
-            } else {
-                res.send(result);
-            }
-        })
-    }
-})
-
-server.put('/editclick/:taskid',(req,res)=>{
-    const taskid = req.params.taskid
-    connection.query(`UPDATE ${database}.todotask SET editON = editON +1 WHERE taskid = ?;`,taskid,
-    (err,result)=>{
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    }
+      }
     )
-}
-)
+  } else return
+})
+
+server.get('/', (req, res) => {
+  connection.query(`SELECT * FROM ${database}.todotask;`, (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(result)
+    }
+  })
+})
+
+server.put('/complete/:taskid', (req, res) => {
+  const taskid = req.params.taskid
+  connection.query(
+    `UPDATE ${database}.todotask SET complete = complete +1 WHERE taskid = ?`,
+    taskid,
+    (err, result) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send(result)
+      }
+    }
+  )
+})
+
+server.delete('/deletetodos/:taskid', (req, res) => {
+  const taskid = req.params.taskid
+  connection.query(
+    `DELETE FROM ${database}.todotask WHERE (taskid = ?)`,
+    taskid,
+    (err, result) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send(result)
+      }
+    }
+  )
+})
+
+server.put('/editsumbit/:taskid', (req, res) => {
+  const taskid = req.params.taskid
+  const editTodos = req.body.editTodos
+  if (editTodos !== '') {
+    connection.query(
+      `UPDATE ${database}.todotask SET tasks = ?, editON = editON +1 WHERE taskid = ?;`,
+      [editTodos, taskid],
+      (err, result) => {
+        if (err) {
+          console.log(err)
+        } else {
+          res.send(result)
+        }
+      }
+    )
+  }
+})
+
+server.put('/editclick/:taskid', (req, res) => {
+  const taskid = req.params.taskid
+  connection.query(
+    `UPDATE ${database}.todotask SET editON = editON +1 WHERE taskid = ?;`,
+    taskid,
+    (err, result) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send(result)
+      }
+    }
+  )
+})
